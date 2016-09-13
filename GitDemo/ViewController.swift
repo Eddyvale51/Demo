3ade9d000124c25f416ecf1c78ab61c12d525eb7
Eduard Valeisha <Eddy@eduards-imac.att.net>
2016-09-13 12:07:36 -0400

    Initial Commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Eduard Valeisha on 9/13/16.
+//  Copyright © 2016 Eduard Valeisha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
